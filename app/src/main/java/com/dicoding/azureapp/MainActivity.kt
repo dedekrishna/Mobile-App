5b95c9c3d1ea5b7fd7de92935b278383656e8b03
dedekrishna <dedekrishna963@gmail.com>
2020-01-04 21:52:08 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/azureapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.dicoding.azureapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var heroes: ArrayList<Hero> = arrayListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        AppCenter.start(
+            application, "94bab5b9-bdaa-4d03-a7f1-61a5264bf372",
+            Analytics::class.java, Crashes::class.java
+        )
+
+        heroes.addAll(HeroesData.listData)
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        rv_hero.layoutManager = LinearLayoutManager(this)
+        rv_hero.adapter = ListHeroAdapter(heroes)
+    }
+}
